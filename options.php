1fcbc5d467c4b12dc8fd6c09d5b0322d6029601f
bismo_softwareseni <bismo@softwareseni.com>
2019-07-26 10:21:47 +0700

    create child theme 3 with options upload logo and enabling maintenance mode

--- options.php
+++ options.php
@@ -28,51 +28,31 @@ function optionsframework_options() {
 
 	//-- heading tab for basic setting
 	$options[] = array(
-		'name' => __( 'Basic Settings', 'ssblanktheme_child_2' ),
+		'name' => __( 'Basic Settings', 'ssblanktheme_child_3' ),
 		'type' => 'heading'
 	);
 
 	//-- upload logo
 	$options[] = array(
-		'name' 	=> __( 'Logo', 'ssblanktheme_child_2' ),
-		'desc' 	=> __( 'Please select a logo for your site', 'ssblanktheme_child_2' ),
+		'name' 	=> __( 'Logo', 'ssblanktheme_child_3' ),
+		'desc' 	=> __( 'Please select a logo for your site', 'ssblanktheme_child_3' ),
 		'id' 	=> 'ss_blank_logo',
 		'type' 	=> 'upload'
 	);
 
-	//-- sidebars to show on frontpage
-	$ss_sidebars_options = array();
-
-	//-- check box values
-	$ss_sidebars_options[ 0 ] = array(
-		'ssbt-sidebar-left' 	=> __('Left Sidebar', 'ssblanktheme_child_2'),
-		'ssbt-sidebar-right' 	=> __('Right Sidebar', 'ssblanktheme_child_2')
-	);
-
-	//-- default sidebars to show
-	$ss_sidebars_options[ 1 ] = array(
-		'ssbt-sidebar-left' 	=> '0',
-		'ssbt-sidebar-right' 	=> '1'
+	//-- maintenance mode or normal mode
+	$ss_maintenance_mode_options = array(
+		'1' => __('Yes', 'ssblanktheme_child_3'),
+		'0' => __('No', 'ssblanktheme_child_3')
 	);
 
 	$options[] = array(
-		'name' 		=> __('Sidebars To Show On Frontpage', 'ssblanktheme_child_2'),
-		'desc' 		=> __('Please choose which sidebar that you want to show on frontpage', 'ssblanktheme_child_2'),
-		'id' 		=> 'ss_blank_sidebars_visibility',
-		'std' 		=> $ss_sidebars_options[ 1 ],
-		'type' 		=> 'multicheck',
-		'options' 	=> $ss_sidebars_options[ 0 ]
-	);
-	//-- end sidebars to show on frontpage
-
-
-	//-- limit posts to show on frontpage
-	$options[] = array(
-		'name' => __('Max amount of posts to show', 'ssblanktheme_child_2'),
-		'desc' => __('Limit posts to show on the frontpage', 'ssblanktheme_child_2'),
-		'id' => 'ss_blank_post_limit',
-		'std' => '5',
-		'type' => 'text'
+		'name' => __('Enabling Maintenance Mode?', 'ssblanktheme_child_3'),
+		'desc' => __('Please select yes if you want to enabling the maintenance mode', 'ssblanktheme_child_3'),
+		'id' => 'ss_blank_maintenance_mode',
+		'std' => '0',
+		'type' => 'radio',
+		'options' => $ss_maintenance_mode_options
 	);
 
 	return $options;
